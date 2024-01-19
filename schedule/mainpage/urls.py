28e6685493aab817344846bedd523d116b84fb7a
CectAss <78093532+CectAss@users.noreply.github.com>
2024-01-20 00:47:54 +0300

    Add other pages

--- schedule/mainpage/urls.py
+++ schedule/mainpage/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('studentpage', views.studentpage),
     path('', views.index)
 ]
--- schedule/mainpage/views.py
+++ schedule/mainpage/views.py
@@ -1,6 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 def index(request):
-    #return HttpResponse("<h4>МАКСИМ ГОМОСЕК!!</h4>")
+    # return HttpResponse("<h4>МАКСИМ ГОМОСЕК!!</h4>")
     return render(request, 'mainpage/index.html')
+
+
+def studentpage(request):
+    return render(request, 'mainpage/studentpage.html')
+
+
+
