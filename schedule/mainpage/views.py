bfceddc19bc9015c01aca478d7f6f44e7a4f15f2
CectAss <78093532+CectAss@users.noreply.github.com>
2024-01-19 22:24:25 +0300

    Add main page

--- schedule/mainpage/views.py
+++ schedule/mainpage/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("<h4>МАКСИМ ГОМОСЕК!!</h5>")
+    #return HttpResponse("<h4>МАКСИМ ГОМОСЕК!!</h4>")
+    return render(request, 'mainpage/index.html')
