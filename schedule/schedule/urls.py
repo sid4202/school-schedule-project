ab5b1044bcff759e3ad155a04e891031514996bc
Max XXX <maxonleb5@gmail.com>
2024-01-20 11:42:28 +0300

    Refactored

--- schedule/schedule/urls.py
+++ schedule/schedule/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from . import views
 
 urlpatterns = [
-    path('', include('mainpage.urls'))
+    path('', include('mainPage.urls'))
 ]
